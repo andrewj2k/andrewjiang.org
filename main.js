5f7b008e5975e9662021d6b9a0607ec55b4888e9
Andrew Jiang <andrewj2k@gmail.com>
2025-06-29 22:30:22 -0400

    Him

---
+++ main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const roles = document.getElementById('roles');
+  const words = ['a maker', 'a developer', 'an analyst', 'studying CS at Cornell'];
+  let i = 0;
+  let j = 0;
+  let isDeleting = false;
+  let currentWord = '';
+  
+  function type() {
+    const word = words[i];
+    if (isDeleting) {
+      currentWord = word.substring(0, j--);
+    } else {
+      currentWord = word.substring(0, j++);
+    }
+
+    roles.textContent = currentWord;
+
+    let delay = isDeleting ? 80 : 120;
+
+    if (!isDeleting && currentWord === word) {
+      delay = 1500;
+      isDeleting = true;
+    } else if (isDeleting && currentWord === '') {
+      isDeleting = false;
+      i = (i + 1) % words.length;
+      delay = 500;
+    }
+
+    setTimeout(type, delay);
+  }
+
+  type();
+});
